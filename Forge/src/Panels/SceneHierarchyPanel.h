d16f483512304d44ec4ecc9195d33a08afe068cd
ThatTanishqTak <tanishqtak3@gmail.com>
2024-11-20 21:00:36 +0530

    Refactored some more code

--- Forge/src/Panels/SceneHierarchyPanel.h
+++ Forge/src/Panels/SceneHierarchyPanel.h
@@ -22,6 +22,9 @@ namespace Trinity
 		void SetSelectedEntity(Entity entity);
 
 	private:
+		template<typename T>
+		void DisplayAddComponentEntry(const std::string& entryName);
+
 		void DrawEntityNode(Entity entity);
 		void DrawComponents(Entity entity);
 
