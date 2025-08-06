fbba55ebc826a6c3b83779ac6621544a4b83970f
ThatTanishqTak <tanishqtak3@gmail.com>
2025-08-06 11:16:49 +0100

    Extended render graph for resource management and added ImGui (docking branch)

--- Trinity/src/Trinity/Renderer/RenderGraph.h
+++ Trinity/src/Trinity/Renderer/RenderGraph.h
@@ -3,6 +3,7 @@
 #include <functional>
 #include <vector>
 #include <string>
+#include <unordered_map>
 
 namespace Trinity
 {
@@ -11,19 +12,55 @@ namespace Trinity
     public:
         using PassFunc = std::function<void()>;
 
+        enum class ResourceType
+        {
+            Texture,
+            Buffer
+        };
+
+        struct Resource
+        {
+            ResourceType Type;
+            bool Transient = false;
+        };
+
+        struct Barrier
+        {
+            std::string Resource;
+            std::string SrcPass;
+            std::string DstPass;
+        };
+
         struct Pass
         {
             std::string Name;
             PassFunc Func;
+            std::vector<std::string> Inputs;
+            std::vector<std::string> Outputs;
             bool Offscreen = false;
         };
 
-        void AddPass(const std::string& name, const PassFunc& func, bool offscreen = false);
+        void AddPass(const std::string& name, const PassFunc& func,
+            const std::vector<std::string>& inputs = {},
+            const std::vector<std::string>& outputs = {},
+            bool offscreen = false);
+
+        void AddPass(const std::string& name, const PassFunc& func, bool offscreen);
+
+        void DeclareTransientTexture(const std::string& name);
+        void DeclareTransientBuffer(const std::string& name);
+
         void Execute();
         void Clear();
 
     private:
-        std::vector<Pass> m_OffscreenPasses;
-        std::vector<Pass> m_OnscreenPasses;
+        void BuildDependencyGraph();
+        std::vector<size_t> TopologicalSort() const;
+        void InsertBarriers(const std::vector<size_t>& order);
+
+        std::vector<Pass> m_Passes;
+        std::vector<Barrier> m_Barriers;
+        std::unordered_map<std::string, Resource> m_Resources;
+        std::vector<std::vector<size_t>> m_DependencyGraph;
     };
 }
