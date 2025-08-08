8c8ac65b50bab41ff8978bd4ee7a6a9437339a3c
ThatTanishqTak <tanishqtak3@gmail.com>
2025-08-08 20:13:06 +0100

    Adding ImGuizmo

--- TrinityForge/src/Panels/SceneViewportPanel.cpp
+++ TrinityForge/src/Panels/SceneViewportPanel.cpp
@@ -1,9 +1,13 @@
 #include "Trinity/trpch.h"
 #include "SceneViewportPanel.h"
+#include "Trinity/ECS/Components.h"
 
+#include <ImGuizmo.h>
 #include <imgui.h>
+#include <glm/gtc/type_ptr.hpp>
 
-SceneViewportPanel::SceneViewportPanel(Trinity::Renderer* renderer) : m_Renderer(renderer)
+SceneViewportPanel::SceneViewportPanel(Trinity::Renderer* renderer, Trinity::Scene* context, entt::entity* selectionContext) : m_Renderer(renderer), m_Context(context), 
+    m_SelectionContext(selectionContext)
 {
 
 }
@@ -21,6 +25,35 @@ void SceneViewportPanel::OnUIRender()
         if (l_TextureID)
         {
             ImGui::Image(l_TextureID, l_ViewportPanelSize);
+        }
+
+        if (m_Context && m_SelectionContext && *m_SelectionContext != entt::null)
+        {
+            auto& l_Registry = m_Context->GetRegistry();
+            if (l_Registry.any_of<Trinity::TransformComponent>(*m_SelectionContext))
+            {
+                ImGuizmo::SetOrthographic(false);
+                ImGuizmo::BeginFrame();
+                ImGuizmo::SetDrawlist();
+                ImGuizmo::SetRect(ImGui::GetWindowPos().x, ImGui::GetWindowPos().y, m_ViewportSize.x, m_ViewportSize.y);
+
+                glm::mat4 l_View = m_Renderer->GetCamera().GetViewMatrix();
+                glm::mat4 l_Projection = m_Renderer->GetCamera().GetProjectionMatrix();
+
+                auto& a_Transform = l_Registry.get<Trinity::TransformComponent>(*m_SelectionContext);
+                glm::mat4 l_Transform = a_Transform.GetTransform();
+
+                ImGuizmo::Manipulate(glm::value_ptr(l_View), glm::value_ptr(l_Projection), ImGuizmo::TRANSLATE, ImGuizmo::LOCAL, glm::value_ptr(l_Transform));
+
+                if (ImGuizmo::IsUsing())
+                {
+                    glm::vec3 l_Translation, l_Rotation, l_Scale;
+                    ImGuizmo::DecomposeMatrixToComponents(glm::value_ptr(l_Transform), glm::value_ptr(l_Translation), glm::value_ptr(l_Rotation), glm::value_ptr(l_Scale));
+                    a_Transform.Translation = l_Translation;
+                    a_Transform.Rotation = l_Rotation;
+                    a_Transform.Scale = l_Scale;
+                }
+            }
         }
     }
 
--- TrinityForge/src/Panels/SceneViewportPanel.h
+++ TrinityForge/src/Panels/SceneViewportPanel.h
@@ -2,17 +2,21 @@
 
 #include "Trinity/UI/Panel.h"
 #include "Trinity/Renderer/Renderer.h"
+#include "Trinity/ECS/Scene.h"
 
+#include <entt/entt.hpp>
 #include <glm/vec2.hpp>
 
 class SceneViewportPanel : public Trinity::Panel
 {
 public:
-    SceneViewportPanel(Trinity::Renderer* renderer);
+    SceneViewportPanel(Trinity::Renderer* renderer, Trinity::Scene* context, entt::entity* selectionContext);
 
     void OnUIRender() override;
 
 private:
     Trinity::Renderer* m_Renderer = nullptr;
+    Trinity::Scene* m_Context = nullptr;
+    entt::entity* m_SelectionContext = nullptr;
     glm::vec2 m_ViewportSize{ 0.0f, 0.0f };
 };
